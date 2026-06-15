89ce9b0d73e94172ac257b06f0b08c73bb65be75
Sergew_RMZ <serge15games@gmail.com>
2026-06-15 01:28:45 -0600

    fibonacci sequence iterative

--- Dynammic_Programming/fibo.cpp
+++ Dynammic_Programming/fibo.cpp
@@ -4,14 +4,26 @@
 using namespace std;
 int fib(int n);
 int fib_memo(int n);
+int fibo_bottom_up(int n);
 
 vector <int> memo;
 
 int main() {
   int n = 1000;
   memo.resize(n + 1, -1);
-  cout << "El valor de n es: " << fib_memo(n) << endl;
+  cout << "El valor de n es: " << fibo_bottom_up(n) << endl;
   return 0;
+}
+
+int fibo_bottom_up(int n) {
+  vector<int> fibo(n);
+  fibo[0] = 1; fibo[1] = 1;
+
+  for (int i = 2; i <= n; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  }
+
+  return fibo[n];
 }
 
 int fib(int n) {
